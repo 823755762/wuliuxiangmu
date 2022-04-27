e61d29ea422f20a4d1877e32ef7c2fa92ddda679
lishuo <714920076@qq.com>
2022-04-27 17:01:06 +0800

    user用户数据更新  2022-04-27

--- src/main/java/com/hz/WuliuxiangmuApplication.java
+++ src/main/java/com/hz/WuliuxiangmuApplication.java
@@ -1,9 +1,11 @@
 package com.hz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hz.mapper")
 public class WuliuxiangmuApplication {
 
     public static void main(String[] args) {
